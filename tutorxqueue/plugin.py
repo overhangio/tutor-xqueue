bbc9dfc5d4ff1461d7b0a803c4db06594500e1db
Régis Behmo <regis@behmo.com>
2019-08-20 18:43:26 +0200

    :sunrise:

---
+++ tutorxqueue/plugin.py
@@ -0,0 +1,41 @@
+from glob import glob
+import os
+
+import pkg_resources
+
+from .__about__ import __version__
+
+
+config = {
+    "add": {
+        "AUTH_PASSWORD": "{{ 8|random_string }}",
+        "MYSQL_PASSWORD": "{{ 8|random_string }}",
+        "SECRET_KEY": "{{ 24|random_string }}",
+    },
+    "defaults": {
+        "VERSION": __version__,
+        "DOCKER_IMAGE": "overhangio/openedx-xqueue:{{ XQUEUE_VERSION }}",
+        "AUTH_USERNAME": "lms",
+        "MYSQL_DATABASE": "xqueue",
+        "MYSQL_USERNAME": "xqueue",
+    },
+}
+
+templates = pkg_resources.resource_filename("tutorxqueue", "templates")
+hooks = {
+    "init": ["mysql-client", "xqueue"],
+    "build-image": {"xqueue": "{{ XQUEUE_DOCKER_IMAGE }}"},
+    "remote-image": {"xqueue": "{{ XQUEUE_DOCKER_IMAGE }}"},
+}
+
+
+def patches():
+    all_patches = {}
+    for path in glob(
+        os.path.join(pkg_resources.resource_filename("tutorxqueue", "patches"), "*")
+    ):
+        with open(path) as patch_file:
+            name = os.path.basename(path)
+            content = patch_file.read()
+            all_patches[name] = content
+    return all_patches
